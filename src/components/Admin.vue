6a532abee8cf78ac7773581c6dfc4ad16d5808dd
hxh <hanxinhu521@163.com>
2019-03-12 16:59:21 +0800

    test

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,58 +1,82 @@
 <template>
   <el-container>
     <el-main>
-        <el-table :data="updates" border style="width: 100%">
-          <el-table-column fixed="left" prop="id" label="ID"></el-table-column>
-          <el-table-column prop="rid" label="restaurant"></el-table-column>
-          <el-table-column label="name">
-            <el-table-column prop="nameBefore" label="before"></el-table-column>
-            <el-table-column prop="nameAfter" label="after"></el-table-column>
-          </el-table-column>
-          <el-table-column label="description">
-            <el-table-column prop="descriptionBefore" label="before"></el-table-column>
-            <el-table-column prop="descriptionAfter" label="after"></el-table-column>
-          </el-table-column>
-          <el-table-column label="address">
-            <el-table-column label="before">
-              <el-table-column prop="provinceBefore" label="province">
-              </el-table-column>
-              <el-table-column prop="cityBefore" label="city"></el-table-column>
-              <el-table-column prop="districtBefore" label="district"></el-table-column>
-              <el-table-column prop="streetBefore" label="street"></el-table-column>
+      <p>approve restaurant for sign up</p>
+      <el-table :data="restaurants" border style="width: 100%">
+        <el-table-column label="restaurant" prop="rid"></el-table-column>
+        <el-table-column label="name" prop="name"></el-table-column>
+        <el-table-column label="description" prop="description"></el-table-column>
+        <el-table-column label="type" prop="type"></el-table-column>
+        <el-table-column label="email" prop="email"></el-table-column>
+        <el-table-column label="address">
+          <el-table-column label="province" prop="province"></el-table-column>
+          <el-table-column label="city" prop="city"></el-table-column>
+          <el-table-column label="district" prop="district"></el-table-column>
+          <el-table-column label="street" prop="street"></el-table-column>
+        </el-table-column>
+        <el-table-column label="options">
+          <template slot-scope="scope">
+            <el-button type="text" @click.native.prevent="approveRest(scope.$index)">approve</el-button>
+            <el-button type="text" @click.native.prevent="rejectRest(scope.$index)">reject</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <p>approve restaurant for update profile</p>
+
+      <el-table :data="updates" border style="width: 100%">
+        <el-table-column fixed="left" prop="id" label="ID"></el-table-column>
+        <el-table-column prop="rid" label="restaurant"></el-table-column>
+        <el-table-column label="name">
+          <el-table-column prop="nameBefore" label="before"></el-table-column>
+          <el-table-column prop="nameAfter" label="after"></el-table-column>
+        </el-table-column>
+        <el-table-column label="description">
+          <el-table-column prop="descriptionBefore" label="before"></el-table-column>
+          <el-table-column prop="descriptionAfter" label="after"></el-table-column>
+        </el-table-column>
+        <el-table-column label="address">
+          <el-table-column label="before">
+            <el-table-column prop="provinceBefore" label="province">
             </el-table-column>
-            <el-table-column label="after">
-              <el-table-column prop="provinceAfter" label="province"></el-table-column>
-              <el-table-column prop="cityAfter" label="city"></el-table-column>
-              <el-table-column prop="districtAfter" label="district"></el-table-column>
-              <el-table-column prop="streetAfter" label="street"></el-table-column>
-            </el-table-column>
+            <el-table-column prop="cityBefore" label="city"></el-table-column>
+            <el-table-column prop="districtBefore" label="district"></el-table-column>
+            <el-table-column prop="streetBefore" label="street"></el-table-column>
           </el-table-column>
-          <el-table-column fixed = "right" label="options">
+          <el-table-column label="after">
+            <el-table-column prop="provinceAfter" label="province"></el-table-column>
+            <el-table-column prop="cityAfter" label="city"></el-table-column>
+            <el-table-column prop="districtAfter" label="district"></el-table-column>
+            <el-table-column prop="streetAfter" label="street"></el-table-column>
+          </el-table-column>
+        </el-table-column>
+        <el-table-column fixed="right" label="options">
+          <template slot-scope="scope">
+            <el-button @click.native.prevent="approve(scope.$index)" type="primary">approve</el-button>
+            <el-button @click.native.prevent="reject(scope.$index)" type="danger">reject</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <p><br>
+        <br></p>
+      <div>
+
+        <el-table :data="settlements" border style="width: 100%">
+          <el-table-column label="restaurant" prop="rid"></el-table-column>
+          <el-table-column label="bill" prop="id"></el-table-column>
+          <el-table-column label="time" prop="receiveTime"></el-table-column>
+          <el-table-column label="sum" prop="sum"></el-table-column>
+          <el-table-column label="options">
             <template slot-scope="scope">
-              <el-button @click.native.prevent="approve(scope.$index)" type="primary">approve</el-button>
-              <el-button @click.native.prevent="reject(scope.$index)" type="danger">reject</el-button>
+              <el-button @click.native.prevent="settle(scope.$index)" type="text">settle account</el-button>
             </template>
           </el-table-column>
         </el-table>
-        <p><br>
-        <br></p>
-        <div>
-          <el-table :data="settlements" border style="width: 100%">
-            <el-table-column label="restaurant" prop="rid"></el-table-column>
-            <el-table-column label="bill" prop="id"></el-table-column>
-            <el-table-column label="time" prop="receiveTime"></el-table-column>
-            <el-table-column label="sum" prop="sum"></el-table-column>
-            <el-table-column label="options">
-              <template slot-scope="scope">
-                <el-button @click.native.prevent="settle(scope.$index)" type="text">settle account</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
-          <el-button type="primary" @click="settleAll()">
-            Settle Account All
-          </el-button>
-          <div>balance : {{balance}}</div>
-        </div>
+        <el-button type="primary" @click="settleAll()">
+          Settle Account All
+        </el-button>
+        <div>balance : {{balance}}</div>
+      </div>
     </el-main>
   </el-container>
 </template>
@@ -75,30 +99,60 @@
   export default {
     name: 'Admin',
     created () {
+      this.getRestaurants()
       this.getUpdates()
       this.getSettlements()
     },
     data () {
       return {
+        restaurants: [],
         updates: [],
         settlements: [],
         balance: 0,
       }
     },
     methods: {
+      approveRest (row) {
+        this.updateRest('normal', row)
+      },
+      rejectRest (row) {
+        this.updateRest('rejected', row)
+      },
+      updateRest (str, row) {
+        let restaurant = this.restaurants[row]
+        this.restaurants.splice(row, 1)
+        restaurant.state = str
+        let http = new XMLHttpRequest()
+        let path = app.path() + '/restaurant/update'
+        http.open('POST', path)
+        http.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+        http.send(JSON.stringify(restaurant))
+      },
+      getRestaurants () {
+        let _this = this
+        let http = new XMLHttpRequest()
+        let path = app.path() + '/restaurant/getWaiting'
+        http.open('GET', path, true)
+        http.send(null)
+        http.onreadystatechange = function () {
+          if (http.status === 200 && http.readyState === 4) {
+            _this.restaurants = JSON.parse(http.responseText)
+          }
+        }
+      },
       approve (row) {
-        this.modify("approved",row)
+        this.modify('approved', row)
       },
       reject (row) {
-        this.modify("rejected",row)
+        this.modify('rejected', row)
       },
-      modify(str,row){
+      modify (str, row) {
         let infoUpdate = this.updates[row]
-        this.updates.splice(row,1)
+        this.updates.splice(row, 1)
         infoUpdate.state = str
         let http = new XMLHttpRequest()
-        let path = app.path() + "/infoUpdate/update"
-        http.open('POST',path,true)
+        let path = app.path() + '/infoUpdate/update'
+        http.open('POST', path, true)
         http.setRequestHeader('Content-type', 'application/json; charset=utf-8')
         http.send(JSON.stringify(infoUpdate))
       },
@@ -110,11 +164,11 @@
         http.send(null)
         http.onreadystatechange = function () {
           if (http.readyState === 4 && http.status === 200) {
-            _this.updates = JSON.parse(http.responseText);
+            _this.updates = JSON.parse(http.responseText)
           }
         }
       },
-      getSettlements(){
+      getSettlements () {
         let _this = this
         let http = new XMLHttpRequest()
         let address = app.path() + '/admin/getSettlements'
@@ -122,37 +176,37 @@
         http.send(null)
         http.onreadystatechange = function () {
           if (http.readyState === 4 && http.status === 200) {
-            _this.settlements = JSON.parse(http.responseText);
+            _this.settlements = JSON.parse(http.responseText)
           }
         }
       },
-      settleAll(){
-        for(var i = 0 ; i < this.settlements.length;i++){
-          settlements[i].settled = true;
+      settleAll () {
+        for (var i = 0; i < this.settlements.length; i++) {
+          settlements[i].settled = true
         }
         let http = new XMLHttpRequest()
         let address = app.path() + '/admin/settle'
-        http.open('POST',address,true)
+        http.open('POST', address, true)
         http.setRequestHeader('Content-type', 'application/json; charset=utf-8')
         http.send(JSON.stringify(this.settlements))
       },
-      settle(row){
+      settle (row) {
         this.settlements[row].settled = true
         let http = new XMLHttpRequest()
         let address = app.path() + '/admin/settle'
-        http.open('POST',address,true)
+        http.open('POST', address, true)
         http.setRequestHeader('Content-type', 'application/json; charset=utf-8')
         http.send(JSON.stringify(this.settlements[row]))
-        this.openSuccess();
+        this.openSuccess()
       },
-      openSuccess(){
+      openSuccess () {
         this.$notify({
-          title:'Success',
-          message :'done perfectly',
-          type : 'success'
+          title: 'Success',
+          message: 'done perfectly',
+          type: 'success'
         })
       },
-      getBalance(){
+      getBalance () {
         let _this = this
         let http = new XMLHttpRequest()
         let address = app.path() + '/admin/getBalance'
